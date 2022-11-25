2fc146fe69fb992760070fa2218373b2f2ea1051
Grant <grant_dude1@hotmail.com>
2022-11-25 19:01:05 +0200

    Changed the landing page to the SignUpPage. Landing page is now the AboutUsPage.

--- src/Nav.js
+++ src/Nav.js
@@ -19,17 +19,23 @@ const Nav = () => {
               <a href="/Members" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Members</a>
               <a href="/GalleryPage" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Gallery</a>
               <a href="/ContactPage" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Contact Us</a>
+              <a href="/SignUpPage" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Sign Up</a>
             </div>
           </div>
         </div>
         <div class="hidden md:block">
           <div class="ml-4 flex items-center md:ml-6">
+            {/* 
             <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700" aria-label="Notifications">
               <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
               </svg>
             </button>
-
+            */}
+            <div class="ml-3 relative">
+            <a href="/SignInPage" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Sign In</a>
+            </div>
+            {/*
             <div class="ml-3 relative">
               <button class="max-w-xs flex items-center text-sm rounded-full text-white focus:outline-none focus:shadow-solid" id="user-menu" aria-label="User menu" aria-haspopup="true">
                 <img class="h-8 w-8 rounded-full" src="/images/profileimage.jpg" alt="" />
@@ -38,11 +44,11 @@ const Nav = () => {
                 <div class="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
                   <a href="/ProfilePage" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
                   <a href="/DashBoard" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">DashBoard</a>
-                  
-                    <a href="/#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign Out</a>
+                  <a href="/#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign Out</a>
                 </div>
               </div>
             </div>
+            */}
           </div>
         </div>
         <div class="-mr-2 flex md:hidden">
--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,53 +1,53 @@
 const SignUpPage = () => {
     return (
-        <main class="p-10 bg-zinc-200">
-<div class="bg-grey-lighter min-h-screen flex flex-col">
-            <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    <h1 class="mb-8 text-3xl text-center">Sign up</h1>
-                    <input 
-                        type="text"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="fullname"
-                        placeholder="Full Name" />
-                        
-                    <input
-                        type="text"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="email"
-                        placeholder="Email"
-                         />
+        <main class="p-10 bg-gray-900">
+            <div class="bg-grey-lighter min-h-screen flex flex-col">
+                <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                    <div class="bg-gray-900 px-6 py-8 rounded shadow-md text-white w-full">
+                        <h1 class="mb-8 text-3xl text-center underline">Sign up</h1>
+                        <input 
+                            type="text"
+                            class="block border border-white-light w-full p-3 rounded mb-4"
+                            name="fullname"
+                            placeholder="Full Name" />
+                            
+                        <input
+                            type="text"
+                            class="block border border-white-light w-full p-3 rounded mb-4"
+                            name="email"
+                            placeholder="Email"
+                            />
 
-                    <input 
-                        type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="password"
-                        placeholder="Password"
-                         />
-                    <button
-                        type="submit"
-                        class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-                    >Create Account</button>
+                        <input 
+                            type="password"
+                            class="block border border-white-light w-full p-3 rounded mb-4"
+                            name="password"
+                            placeholder="Password"
+                            />
+                        <button
+                            type="submit"
+                            class="w-full bg-indigo-600 text-center py-3 rounded text-white hover:bg-indigo-700 focus:outline-none my-1"
+                        >Create Account</button>
 
-                    <div class="text-center text-sm text-grey-dark mt-4">
-                        By signing up, you agree to the 
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#/">
-                            Terms of Service
-                        </a> and 
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#/">
-                            Privacy Policy
-                        </a>
+                        <div class="text-center text-sm text-white mt-4">
+                            By signing up, you agree to the &nbsp;
+                            <a class="no-underline border-b border-grey-dark text-grey-dark" href="#/">
+                                Terms of Service
+                            </a> and &nbsp; 
+                            <a class="no-underline border-b border-grey-dark text-grey-dark" href="#/">
+                                Privacy Policy
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="text-white mt-6">
+                        Already have an account? &nbsp;
+                        <a class="no-underline border-b border-blue text-blue" href="../login/">
+                            Log in
+                        </a>.
                     </div>
                 </div>
-
-                <div class="text-grey-dark mt-6">
-                    Already have an account? 
-                    <a class="no-underline border-b border-blue text-blue" href="../login/">
-                        Log in
-                    </a>.
-                </div>
             </div>
-        </div>
         </main>
     )
 }
