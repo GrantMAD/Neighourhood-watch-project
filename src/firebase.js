a8eadf1a7ccb9bb9baef3fe02b739975359e6896
Grant <grant_dude1@hotmail.com>
2022-11-30 14:48:15 +0200

    added conditional renderingwhen logged in

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,14 @@ const firebaseConfig = {
     projectId: "neighbourhood-watch-project",
     storageBucket: "neighbourhood-watch-project.appspot.com",
     messagingSenderId: "612131927235",
-    appId: "1:612131927235:web:58b0e7b9dc5bd1a4595e76"
+    appId: "1:612131927235:web:58b0e7b9dc5bd1a4595e76",
+    dataBaseUrl:"https://https://neighbourhood-watch-project-default-rtdb.firebaseio.com/"
   };
 
   const app = initializeApp(firebaseConfig);
-
+  
   export const auth = getAuth(app);
+  
 
 
 
