30f664735e0f41bb7bf9f5de13e5374731c6d12a
Grant <grant_dude1@hotmail.com>
2023-05-25 13:51:20 +0200

    corrected alignment issues on md and sm sizes

--- src/SignInPage.js
+++ src/SignInPage.js
@@ -35,65 +35,74 @@ const SignInPage = (props) => {
   })
 
   return (
-    <div className="flex flex-row bg-gray-900">
-      <div className="bg-grey-lighter min-h-screen flex flex-col items-center justify-center w-1/2 text-white border-r border-zinc-200">
-        <section className="flex w-[20rem] flex-col space-y-10">
-        <img src="/images/nwLogo.png" alt="" className="scale-75 shadow-white"/>
-          <h1 className="text-3xl text-center text-zinc-200 font-semibold">Sign In</h1>
-          <div className="w-full transform text-lg duration-300 focus-within:border-indigo-500">
-            <input
-              type="text"
-              placeholder="Email or Username"
-              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none rounded-lg"
-              onChange={(event) => {
-                setLoginEmail(event.target.value);
-              }}
-            />
-          </div>
-
-          <div className="w-full transform text-lg duration-300 focus-within:border-indigo-500">
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none rounded-lg"
-              onChange={(event) => {
-                setLoginPassword(event.target.value);
-              }}
-            />
-          </div>
-
-          <Toaster richColors />
-          <button
-            className="transform rounded-lg bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-700"
-            onClick={() => {
-              login();
-              toast.success('Login successfull')
-            }}
-          >
-            LOG IN
-          </button>
-          {showAlert &&
-            <div>
-              <div class="flex bg-red-200 rounded-lg p-4 mb-4 text-sm text-red-700" role="alert">
-                <svg class="w-5 h-5 inline mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-                <div>
-                  <span class="font-medium">Incorrect Email or password, Please try again</span>
-                </div>
+    <div className="p-16 bg-gray-800">
+      <div className="flex flex-row bg-zinc-200">
+        <div className="bg-grey-lighter min-h-screen flex flex-col items-center justify-center text-white w-full sm:w-1/2">
+          <div className="flex w-[20rem] flex-col space-y-10">
+            <img src="/images/nwLogo.png" alt="" className="scale-75" />
+            <h1 className="text-3xl text-center text-gray-800 font-semibold">Sign In</h1>
+            <div className="md:px-3">
+              <div className="flex items-center justify-center mb-5">
+                <input
+                  type="text"
+                  placeholder="Email or Username"
+                  className="lg:w-full border-2 border-indigo-500 bg-transparent outline-none placeholder:italic focus:outline-none rounded-lg"
+                  onChange={(event) => {
+                    setLoginEmail(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="flex items-center justify-center">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="lg:w-full border-2 border-indigo-500 bg-transparent outline-none placeholder:italic focus:outline-none rounded-lg"
+                  onChange={(event) => {
+                    setLoginPassword(event.target.value);
+                  }}
+                />
+              </div>
+              <Toaster richColors />
+              <div className="flex items-center justify-center md:px-10">
+                <button
+                  className="transform rounded-lg bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-700 lg:w-full md:w-full w-3/5 mt-5"
+                  onClick={() => {
+                    login();
+                    toast.success('Login successfull')
+                  }}
+                >
+                  LOG IN
+                </button>
               </div>
             </div>
-          }
+            {showAlert &&
+              <div>
+                <div class="flex bg-red-200 rounded-lg p-4 mb-4 text-sm text-red-700" role="alert">
+                  <svg class="w-5 h-5 inline mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                  <div>
+                    <span class="font-medium">Incorrect Email or password, Please try again</span>
+                  </div>
+                </div>
+              </div>
+            }
 
-          <a
-            href="/PasswordResetEmail"
-            className="transform text-center font-semibold text-zinc-200 duration-300 hover:text-zinc-200"
-          >FORGOT PASSWORD?</a
-          >
-        </section>
-      </div>
-      <div className="flex flex-col justify-center items-center w-1/2">
-
+            <a
+              href="/PasswordResetEmail"
+              className="transform text-center font-semibold text-gray-800 duration-300 hover:text-gray-900"
+            >FORGOT PASSWORD?</a
+            >
+          </div>
+        </div>
+        <div className="hidden sm:flex flex-col justify-center items-center w-1/2 bg-gradient-to-b from-indigo-500 to-purple-500">
+          <div className="flex flex-col items-center">
+            <h1 className="text-zinc-200 text-xl mb-3">Nice to see you again</h1>
+            <h1 className="text-zinc-200 text-5xl font-medium underline underline-offset-8 md:text-center">WELCOME BACK</h1>
+            <p className="text-center mr-[25%] ml-[25%] mt-10 text-zinc-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus ante, pharetra id semper a, pulvinar id nulla. Sed elementum volutpat felis, sed faucibus orci auctor at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eget nisl tristique, efficitur tellus vel, elementum dui. Morbi elementum libero in felis faucibus, ac ullamcorper massa dapibus. Fusce.</p>
+          </div>
+        </div>
       </div>
     </div>
+
   )
 }
 
--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -44,33 +44,35 @@ const SignUpPage = (props) => {
     }
 
     return (
-        <main>
-            <div className="flex flex-row bg-gray-900">
-                <div className="flex flex-col justify-center items-center w-1/2 border-r border-zinc-200">
-                    <div>
-                        <img src="#" alt="" />
+        <div className="p-16 bg-gray-800">
+            <div className="flex flex-col sm:flex-row bg-zinc-200">
+                <div className="hidden sm:flex flex-col justify-center items-center w-1/2 bg-gradient-to-b from-indigo-500 to-purple-500">
+                    <div className="flex flex-col items-center">
+                        <h1 className="text-zinc-200 text-xl mb-3">Nice to see you</h1>
+                        <h1 className="text-zinc-200 text-5xl font-medium underline underline-offset-8">WELCOME</h1>
+                        <p className="text-center mr-[25%] ml-[25%] mt-10 text-zinc-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus ante, pharetra id semper a, pulvinar id nulla. Sed elementum volutpat felis, sed faucibus orci auctor at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eget nisl tristique, efficitur tellus vel, elementum dui. Morbi elementum libero in felis faucibus, ac ullamcorper massa dapibus. Fusce.</p>
                     </div>
                 </div>
-                <div className="bg-grey-lighter min-h-screen flex flex-col items-center w-1/2">
+                <div className="bg-grey-lighter min-h-screen flex flex-col items-center justify-center text-white w-full sm:w-1/2">
                     <div className="container max-w-sm flex-1 flex flex-col items-center justify-center px-2">
-                        <div className="bg-gray-900 px-8 py-8 rounded-lg text-white w-full">
-                            <img src="/images/nwLogo.png" alt="" className="scale-75 shadow-white"/>
-                            <h1 className="mb-8 text-3xl text-center text-zinc-200 font-semibold">Sign up</h1>
+                        <div className="px-8 py-8 rounded-lg text-white w-full">
+                            <img src="/images/nwLogo.png" alt="" className="scale-75" />
+                            <h1 className="mb-8 text-3xl text-center text-gray-900 font-semibold">Sign up</h1>
                             <div>
                                 <input
                                     type="text"
-                                    className="placeholder-black w-full p-3 mb-4 text-black rounded-lg"
+                                    className="border-2 border-indigo-500 placeholder-black w-full mb-4 text-black rounded-lg"
                                     name="fullname"
                                     placeholder="Full Name"
                                     onChange={(event) => {
                                         setNewName(event.target.value);
                                     }}
                                 />
-    
+
 
                                 <input
                                     type="email"
-                                    className="placeholder-black w-full p-3 mb-4 text-black rounded-lg"
+                                    className="border-2 border-indigo-500 placeholder-black w-full mb-4 text-black rounded-lg"
                                     name="email"
                                     placeholder="Email"
                                     onChange={(event) => {
@@ -90,7 +92,7 @@ const SignUpPage = (props) => {
 
                                 <input
                                     type="password"
-                                    className="placeholder-black w-full p-3 mb-4 text-black rounded-lg"
+                                    className="border-2 border-indigo-500 placeholder-black w-full mb-4 text-black rounded-lg"
                                     name="password"
                                     placeholder="Password"
                                     onChange={(event) => {
@@ -108,7 +110,7 @@ const SignUpPage = (props) => {
                                 }}
 
                             >Create Account</button>
-                            <div className="text-zinc-200 font-semibold text-center mt-3">
+                            <div className="text-gray-800 font-semibold text-center mt-3">
                                 Already have an account? &nbsp;
                                 <a className="no-underline text-indigo-500" href="/SignInPage">
                                     Log in
@@ -119,7 +121,7 @@ const SignUpPage = (props) => {
                     </div>
                 </div>
             </div>
-        </main>
+        </div>
     )
 }
 
