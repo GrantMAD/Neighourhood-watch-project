18f2c00a94c92c6cd625a58da03fdb152184c5d1
Grant <grant_dude1@hotmail.com>
2023-07-30 21:43:30 +0200

    fixed issue with useEffect

--- src/Components/MainStoryPage.js
+++ src/Components/MainStoryPage.js
@@ -21,28 +21,33 @@ const MainStoryPage = () => {
     const usersCollectionRef = collection(db, "users");
 
     useEffect(() => {
-        const fetchStorys = async () => {
-            const storyCollectionRef = collection(db, 'storys');
-            const storyQuery = query(storyCollectionRef, orderBy("timestamp", "desc"));
-            const storyData = await getDocs(storyQuery);
-            setStorys(storyData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-            setIsLoading(false);
-        };
-
         const currentUser = auth.currentUser;
         if (currentUser) {
-            const userEmail = currentUser.email;
-            const userRef = query(usersCollectionRef, where("email", "==", userEmail));
-            onSnapshot(userRef, (snapshot) => {
-                snapshot.forEach((doc) => {
-                    const userData = doc.data();
-                    setUserRole(userData.role);
-                });
+          const userEmail = currentUser.email;
+          const userRef = query(usersCollectionRef, where('email', '==', userEmail));
+          const unsubscribe = onSnapshot(userRef, (snapshot) => {
+            snapshot.forEach((doc) => {
+              const userData = doc.data();
+              setUserRole(userData.role);
             });
+          });
+    
+          // Cleanup the listener when the component unmounts
+          return () => unsubscribe();
         }
+      }, [usersCollectionRef]);
 
+      useEffect(() => {
+        const fetchStorys = async () => {
+          const storyCollectionRef = collection(db, 'storys');
+          const storyQuery = query(storyCollectionRef, orderBy('timestamp', 'desc'));
+          const storyData = await getDocs(storyQuery);
+          setStorys(storyData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+          setIsLoading(false);
+        };
+    
         fetchStorys();
-    }, [usersCollectionRef]);
+      }, []);
 
     useEffect(() => {
         // Scroll to the top of the component when it mounts
