ccd35b6736d4e394a067c70e7fb3f68d7c219090
Grant <grant_dude1@hotmail.com>
2023-08-01 14:03:55 +0200

    hard coded firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  apiKey: "AIzaSyCcEwM1nvju0HIPSQCBqWKHtdhxwaVbe5c",
+  authDomain: "neighbourhood-watch-project.firebaseapp.com",
+  projectId: "https://neighbourhood-watch-project-default-rtdb.firebaseio.com",
+  storageBucket: "neighbourhood-watch-project",
+  messagingSenderId: "neighbourhood-watch-project.appspot.com",
+  appId: "612131927235",
+  databaseURL: "1:612131927235:web:58b0e7b9dc5bd1a4595e76",
 };
 
 const app = initializeApp(firebaseConfig);
