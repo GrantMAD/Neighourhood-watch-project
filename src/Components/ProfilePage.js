7dfc01a3c0393c26f9bdc89abdfb8b14496517a2
Grant <grant_dude1@hotmail.com>
2023-08-05 17:09:25 +0200

    nimor change

--- src/Components/ProfilePage.js
+++ src/Components/ProfilePage.js
@@ -3,7 +3,7 @@ import ProfileForm from './ProfileForm';
 const ProfilePage = () => {
 
     return (
-        <main className="h-screen p-10 bg-zinc-200">
+        <main className="min-h-screen p-10 bg-zinc-200">
             <h1 className="grid text-4xl place-content-center font-semibold underline underline-offset-8 decoration-2 decoration-blue-700 mb-10 mt-8 pt-10">Profile</h1>
             <ProfileForm />
         </main>
