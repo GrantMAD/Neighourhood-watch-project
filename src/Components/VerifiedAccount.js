344e83c57ceb8cc6dfca2e1db8795edc8b6d7fe0
Grant <grant_dude1@hotmail.com>
2023-08-06 17:25:46 +0200

    added email verification

---
+++ src/Components/VerifiedAccount.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom";
+
+const VerifiedAccount = (props) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (props.funcNav) {
+            props.funcNav(false)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
+const SignInRedirect = () => {
+    navigate('/SignInPage')
+}
+
+    return (
+        <main className="mx-auto flex flex-col min-h-screen w-full items-center justify-center bg-gray-900 text-white">
+            <p>Your Account has been verified</p>
+            <button className="bg-gradient-to-l from-blue-800 to-violet-600 hover:bg-gradient-to-r hover:scale-105 p-3 mt-3 rounded-md" onClick={SignInRedirect}>
+                Back to Sign in
+            </button>
+        </main>
+    )
+}
+
+export default VerifiedAccount;
