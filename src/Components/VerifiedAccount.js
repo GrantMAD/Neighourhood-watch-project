ced26020ccfa7d5ed471b351cdff54e53e4494b6
Grant <grant_dude1@hotmail.com>
2023-08-06 17:35:20 +0200

    added logo to verifiedAccount

--- src/Components/VerifiedAccount.js
+++ src/Components/VerifiedAccount.js
@@ -8,15 +8,20 @@ const VerifiedAccount = (props) => {
         if (props.funcNav) {
             props.funcNav(false)
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-const SignInRedirect = () => {
-    navigate('/SignInPage')
-}
+    const SignInRedirect = () => {
+        navigate('/SignInPage')
+    }
 
     return (
         <main className="mx-auto flex flex-col min-h-screen w-full items-center justify-center bg-gray-900 text-white">
+            <div className="flex-shrink-0">
+                <a href="/">
+                    <img className="h-20 scale-75 hover:opacity-75 hover:scale-125 shadow-white" src="/images/nwLogo.png" alt="#" />
+                </a>
+            </div>
             <p>Your Account has been verified</p>
             <button className="bg-gradient-to-l from-blue-800 to-violet-600 hover:bg-gradient-to-r hover:scale-105 p-3 mt-3 rounded-md" onClick={SignInRedirect}>
                 Back to Sign in
