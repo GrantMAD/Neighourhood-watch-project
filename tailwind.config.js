fc91e68bbcdcd9d97429c622def792b1e26f75f2
Grant <grant_dude1@hotmail.com>
2023-08-29 16:08:10 +0200

    added comments for reports

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        "max-height": "max-height",
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
