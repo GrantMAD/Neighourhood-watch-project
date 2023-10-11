025bf02107152777f9016880c332ebf768a68921
Grant <grant_dude1@hotmail.com>
2023-10-11 16:57:00 +0200

    added decline button for requests and remove button for SectorOptions

--- src/AddAndEdit/AddNeighbourhoodRequest.js
+++ src/AddAndEdit/AddNeighbourhoodRequest.js
@@ -1,6 +1,6 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { db } from "../firebase";
-import { collection, addDoc, updateDoc } from "firebase/firestore";
+import { collection, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { Toaster, toast } from 'sonner';
@@ -11,14 +11,32 @@ const AddNeighbourhoodRequest = () => {
     const [postalCode, setPostalCode] = useState();
     const [neighbourhoodName, setNeighbourhoodName] = useState();
     const [roadNames, setRoadNames] = useState();
+    const [neighbourhoodOptions, setNeighbourhoodOptions] = useState([]);
+    const [selectedNeighbourhoodOption, setSelectedNeighbourhoodOption] = useState("");
+    const [refresh, setRefresh] = useState(false);
     const usersCollecctionRef = collection(db, "requests");
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const fetchNeighbourhoodOptions = async () => {
+            const sectorOptionsRef = doc(db, "SectorOptions", "kEbia4X43HuZk6d1FsBp");
+            const sectorOptionsSnapshot = await getDoc(sectorOptionsRef);
+            const sectorOptionsData = sectorOptionsSnapshot.data();
+
+            if (sectorOptionsData) {
+                setNeighbourhoodOptions(sectorOptionsData.NeighbourhoodOptions);
+            }
+        };
+
+        fetchNeighbourhoodOptions();
+        setRefresh(false);
+    }, [refresh]);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+
         const roadNamesArray = roadNames.split(',').map(name => name.trim());
-    
+
         try {
             await addDoc(usersCollecctionRef, {
                 userName: userName,
@@ -27,18 +45,18 @@ const AddNeighbourhoodRequest = () => {
                 neighbourhoodName: neighbourhoodName,
                 roadNames: roadNamesArray
             });
-    
+
             setUserName("");
             setContactNumber("");
             setPostalCode("");
             setNeighbourhoodName("");
             setRoadNames("");
-    
+
             // Create a notification for admins
             const auth = getAuth();
             const user = auth.currentUser;
             const notificationsCollectionRef = collection(db, "notifications");
-    
+
             if (user && user.uid) {
                 const notificationDocRef = await addDoc(notificationsCollectionRef, {
                     title: "New Neighbourhood Request",
@@ -48,20 +66,31 @@ const AddNeighbourhoodRequest = () => {
                     role: "admin",
                     type: 'neighbourhoodRequest'
                 });
-    
+
                 const notificationId = notificationDocRef.id;
                 console.log("New notification ID:", notificationId);
-    
+
                 await updateDoc(notificationDocRef, { notificationId });
             }
-    
+
             toast.success('Request sent!');
             navigate('/ProfilePage');
         } catch (error) {
             console.error("Error sending request:", error);
         }
     };
-    
+
+    const handleDeleteNeighbourhoodOption = async () => {
+        const updatedOptions = neighbourhoodOptions.filter(option => option !== selectedNeighbourhoodOption);
+
+        const sectorOptionsRef = doc(db, "SectorOptions", "kEbia4X43HuZk6d1FsBp");
+        await updateDoc(sectorOptionsRef, {
+            NeighbourhoodOptions: updatedOptions,
+        });
+
+        toast.error('Sector Option deleted successfully');
+        setRefresh(true);
+    };
 
     return (
         <div className="min-h-screen bg-zinc-200 md:p-10 lg:pt-24 lg:pb-24 lg:px-10">
@@ -77,8 +106,34 @@ const AddNeighbourhoodRequest = () => {
                                 Here you can request for your neighbourhood to be added to the Sector 2 group. Please fill in all of the input's as all of the information will be required to verify and approve the addtion of your neighbourhood to Sector 2. An Admin will contact the member or owner registering the neighbourhood by phone. Once an admin is able to verify all of the information your neighbourhood will be added to the list.
                             </p>
                         </div>
+                        <div className="mt-5 px-4 sm:px-0">
+                            <div className="col-span-6 sm:col-span-4">
+                                <label htmlFor="NeighbourhoodOptions" className="block text-md font-medium leading-6 text-gray-900">
+                                    Neighbourhood Options
+                                </label>
+                                <p className="text-sm text-gray-600">To remove a neighbourhood from the list select the neighbourhood in the dropdown and click on the remove button</p>
+                                <select
+                                    id="NeighbourhoodOptions"
+                                    name="NeighbourhoodOptions"
+                                    className="mt-1 block w-1/2 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 sm:text-sm"
+                                    value={selectedNeighbourhoodOption}
+                                    onChange={(e) => setSelectedNeighbourhoodOption(e.target.value)}
+                                >
+                                    <option value="">Select Neighbourhood Option</option>
+                                    {neighbourhoodOptions.map((option, index) => (
+                                        <option key={index} value={option}>{option}</option>
+                                    ))}
+                                </select>
+                            </div>
+                            <button
+                                className="bg-red-500 hover:bg-red-600 hover:scale-105 hover:drop-shadow-2xl text-zinc-200 font-bold py-2 px-4 rounded mt-2"
+                                onClick={handleDeleteNeighbourhoodOption}
+                            >
+                                Remove
+                            </button>
+                        </div>
                     </div>
-                    <div className="mt-5 md:col-span-2 md:mt-0">
+                    <div className="mt-5 md:col-span-2 md:mt-0 m-4 sm:m-0">
                         <form action="#">
                             <div className="shadow sm:overflow-hidden sm:rounded-md">
                                 <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
@@ -177,7 +232,7 @@ const AddNeighbourhoodRequest = () => {
                                     </div>
                                 </div>
                                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                                <Toaster richColors />
+                                    <Toaster richColors />
                                     <button
                                         className="inline-flex justify-center rounded-md border border-transparent bg-gradient-to-l from-blue-800 to-violet-600 hover:bg-gradient-to-r py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                         onClick={handleSubmit}
